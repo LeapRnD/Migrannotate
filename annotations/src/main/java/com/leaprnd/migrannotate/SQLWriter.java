c8501aee02e8fff1b1b15205a509258de89e8193
Ryan Holdren <ryan@leaprnd.com>
2021-07-09 23:07:29 +0000

    AVA-1583: Rollback more reliably

---
+++ annotations/src/main/java/com/leaprnd/migrannotate/SQLWriter.java
@@ -0,0 +1,51 @@
+package com.leaprnd.migrannotate;
+
+import static java.lang.String.format;
+
+public final class SQLWriter {
+
+	private final StringBuilder prologue = new StringBuilder();
+	private final StringBuilder sql = new StringBuilder();
+	private final StringBuilder epilogue = new StringBuilder();
+
+	public void appendToPrologue(String format) {
+		prologue.append(format);
+	}
+
+	public void appendToPrologue(String format, Object ... arguments) {
+		prologue.append(format(format, arguments));
+	}
+
+	public void append(char character) {
+		sql.append(character);
+	}
+
+	public void append(String format) {
+		sql.append(format);
+	}
+
+	public void append(String format, Object ... arguments) {
+		sql.append(format(format, arguments));
+	}
+
+	public void appendToEpilogue(String format) {
+		epilogue.append(format);
+	}
+
+	public void appendToEpilogue(String format, Object ... arguments) {
+		epilogue.append(format(format, arguments));
+	}
+
+	public String getPrologue() {
+		return prologue.toString();
+	}
+
+	public String getSql() {
+		return sql.toString();
+	}
+
+	public String getEpilogue() {
+		return epilogue.toString();
+	}
+
+}
